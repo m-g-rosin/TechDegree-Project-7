9b0d947c2af6473adfa564652574fa37cf44ba08
m-g-rosin <65581784+m-g-rosin@users.noreply.github.com>
2020-10-27 12:41:19 +1000

    added js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 const alertBanner = document.getElementById("alert");
-
+const bell = document.querySelector(".bell");
+const a = document.getElementById("popup_a");
+const b = document.getElementById("popup_b");
+const navLinks = document.getElementsByClassName("traffic-nav-link");
+const searchField = document.querySelector(".form-field");
+const names = ["Victoria Chambers", "Michael Michaels", "Sarah Victors", "John Johns"];
+const autoDisplay = document.querySelector(".widget-container");
+const switchA = document.getElementById("switch-a");
+const switchB = document.getElementById("switch-b");
+const timezone = document.getElementById("timezone");
 alertBanner.innerHTML = 
 `
 <div class="alert-banner">
@@ -8,6 +17,7 @@ alertBanner.innerHTML =
 </div>
 `
 
+//click to close banner
 alertBanner.addEventListener('click', e => {
     const element = e.target;
     if(element.classList.contains("alert-banner-close")) {
@@ -15,16 +25,78 @@ alertBanner.addEventListener('click', e => {
     }
 });
 
+//bell alert to display popups
+bell.addEventListener('click', e => {
+    if(a.style.display = "none") {
+        a.style.display = "";
+    }
+    if(b.style.display = "none") {
+        b.style.display = "";
+    }
+});
+
 const trafficCanvas = document.getElementById("traffic-chart");
 
-let trafficData = {
+//traffic data
+let trafficWeekly = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
     datasets: [{
         data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
         backgroundColor: 'rgba(116, 119, 191, .3)',
         borderWidth: 1,
+        lineTension: 0,
+        pointBorderWidth: 3,
+        pointBorderColor: 'rgba(116, 119, 191, .3)',
+        pointBackgroundColor: 'white',
+        pointRadius: 10
+    }]
+    
+};
+
+let trafficHourly = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        data: [7, 12, 10, 20, 5, 15, 12, 18, 2, 10, 50],
+        backgroundColor: 'rgba(116, 119, 191, .3)',
+        borderWidth: 1,
+        lineTension: 0,
+        pointBorderWidth: 3,
+        pointBorderColor: 'rgba(116, 119, 191, .3)',
+        pointBackgroundColor: 'white',
+        pointRadius: 10
     }]
 };
+
+let trafficDaily = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        data: [75, 125, 100, 200, 50, 150, 120, 180, 20, 100, 50],
+        backgroundColor: 'rgba(116, 119, 191, .3)',
+        borderWidth: 1,
+        lineTension: 0,
+        pointBorderWidth: 3,
+        pointBorderColor: 'rgba(116, 119, 191, .3)',
+        pointBackgroundColor: 'white',
+        pointRadius: 10
+    }]
+    
+};
+
+let trafficMonthly = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        data: [7510, 11250, 10000, 12000, 15000, 17500, 12500, 18500, 22050, 15000, 25000],
+        backgroundColor: 'rgba(116, 119, 191, .3)',
+        borderWidth: 1,
+        lineTension: 0,
+        pointBorderWidth: 3,
+        pointBorderColor: 'rgba(116, 119, 191, .3)',
+        pointBackgroundColor: 'white',
+        pointRadius: 10
+    }]
+    
+};
+
 
 let trafficOptions = {
     aspectRatio: 2.5,
@@ -45,9 +117,10 @@ let trafficOptions = {
 
 let trafficChart = new Chart(trafficCanvas, {
     type: 'line',
-    data: trafficData,
+    data: trafficWeekly,
     options: trafficOptions
 });
+
 
 const dailyCanvas = document.getElementById("daily-chart");
 
@@ -84,7 +157,7 @@ let dailyChart = new Chart(dailyCanvas, {
 const mobileCanvas = document.getElementById("mobile-chart");
 
 const mobileData = {
-    labeles: ["Desktop", "Tablet", "Phones"],
+    labels: ["Desktop", "Tablet", "Phones"],
     datasets: [{
         label: '# of Users',
         data: [2000, 550, 500],
@@ -93,16 +166,21 @@ const mobileData = {
         '#7477BF',
         '#78CF82',
         '#51B6C8'
-        ]
+        ],
+        
     }]
 };
 
 const mobileOptions = {
     legend: {
         position: 'right',
+        
         labels: {
+            align: 'center',
             boxWidth: 20,
-            fontStyle: 'bold'
+            fontStyle: 'bold',
+            fontSize: 20,
+            padding: 10
         }
     }
 }
@@ -117,6 +195,7 @@ const user = document.getElementById("userField");
 const message = document.getElementById("messageField");
 const send = document.getElementById("send");
 
+//popups to make sure forms are completed
 send.addEventListener('click', () => {
     //ensure user and message fields are filled out
     if (user.value === "" && message.value === "") {
@@ -129,3 +208,110 @@ send.addEventListener('click', () => {
         alert(`Message sucessfully sent to: ${user.value}`);
     }
 });
+
+//fuction to change trafficdata displays
+for(let link of navLinks){
+    link.addEventListener('click', e => {
+        if(e.target.innerHTML == "Daily") {
+             new Chart(trafficCanvas, {
+                type: 'line',
+                data: trafficDaily,
+                options: trafficOptions
+            });
+         }  else if (e.target.innerHTML == "Weekly") {
+                new Chart(trafficCanvas, {
+                type: 'line',
+                data: trafficWeekly,
+                options: trafficOptions
+            });
+         } else if (e.target.innerHTML == "Weekly") {
+                new Chart(trafficCanvas, {
+                type: 'line',
+                data: trafficWeekly,
+                options: trafficOptions
+            });
+         }   else if (e.target.innerHTML == "Hourly") {
+                new Chart(trafficCanvas, {
+                type: 'line',
+                data: trafficHourly,
+                options: trafficOptions
+            });
+         } else if (e.target.innerHTML == "Monthly") {
+                new Chart(trafficCanvas, {
+                type: 'line',
+                data: trafficMonthly,
+                options: trafficOptions
+            });
+         }
+    });
+}
+
+//autocomplete feature
+searchField.addEventListener("input", e => {
+    let input = e.target.value.toUpperCase();
+    let i = 0;
+ 
+    
+    let a = document.createElement("DIV");
+    a.setAttribute("class", "autocomplete-items");
+    closeLists();
+    for(let ele of names) {
+        
+        
+        if(input == ele.substr(0, input.length).toUpperCase()) {
+        
+           console.log(ele.substr(0, input.length))
+            let b = document.createElement("DIV");
+            
+            b.innerHTML = "<strong>" + ele.substr(0, input.length) + "</strong>";
+            b.innerHTML += ele.substr(input.length);
+            b.innerHTML += "<input type='hidden' value='" + ele + "'>";
+            
+            autoDisplay.appendChild(a);
+            a.appendChild(b);
+            closeLists(input, input);
+            i++;
+            b.addEventListener("click", function(e) {
+            
+                document.querySelector(".form-field").value = ele;
+                console.log( ele)
+                closeLists();
+            } );
+            
+        }
+    }
+     
+
+        
+    
+});
+
+document.addEventListener("click", function (e) {
+    closeLists(e.target);
+});
+//strongly borrowed from https://www.w3schools.com/howto/howto_js_autocomplete.asp
+function closeLists(c, input) {
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (c != x[i] && c != input) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+ }
+
+ //save settings to localstorage
+ function save() {	
+    localStorage.setItem("switch-a", switchA.checked);
+    localStorage.setItem("switch-b", switchB.checked);
+    localStorage.setItem("timezone", timezone.value);
+    console.log(("timezone", timezone.value))
+}
+ 
+
+
+  let checked = JSON.parse(localStorage.getItem("switch-a"));
+  let checkedb = JSON.parse(localStorage.getItem("switch-b"));
+  let timeSave = localStorage.getItem("timezone");
+     document.getElementById("switch-a").checked = checked;
+     document.getElementById("switch-b").checked = checkedb;
+     document.getElementById("timezone").value = timeSave;
